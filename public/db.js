fa0e5e468221d62d7da21f0b28392e5e9ab3a758
Michael DiSanto <mdisanto928@gmail.com>
2021-07-15 20:07:18 -0400

    added mutiple request and save record function

--- public/db.js
+++ public/db.js
@@ -4,3 +4,31 @@ window.mozIndexedDB ||
 window.webkitIndexedDB ||
 window.shimIndexedDB;
 
+let db;
+const request = indexedDB.open("budget, 1");
+
+request.onupgradeneeded = ({ target}) => {
+    let db = target.result;
+    db.createObjectStore("pending", { autoIncrement: true});
+};
+
+request.onsuccess = ({ target }) => {
+    db = target.result;
+
+
+if (navigator.onLine) {
+    checkDatabase();
+   }
+};
+
+request.onerror = function (event) {
+    console.log("oh no!" + event.target.errorCode)
+};
+
+function saveRecord(record) {
+    const transaction = db.transaction(["pending"], "readwrite");
+    const store = transaction.onjectStore('pending');
+
+    store.add(record);
+};
+
