426d12eceaa59f69dcac819b55edff719325721c
Michael DiSanto <mdisanto928@gmail.com>
2021-07-15 20:16:08 -0400

    Working on checkDatabase

--- public/db.js
+++ public/db.js
@@ -32,3 +32,12 @@ function saveRecord(record) {
     store.add(record);
 };
 
+function checkDatabase() {
+    const transaction = db.transaction(["pending"], readwrite);
+    const store = transaction.objectStore('pending');
+    const getAll = store.getAll();
+
+    getAll.onsuccess = function () {
+        
+    }
+}
