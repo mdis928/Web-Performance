c030d24ad37f94ccf12850b84a2a684374f17dc7
Michael DiSanto <mdisanto928@gmail.com>
2021-07-15 20:27:08 -0400

    added .then to checkDatabase function

--- public/db.js
+++ public/db.js
@@ -1,3 +1,5 @@
+// const { response } = require("express");
+
 const indexedDB =
 window.indexedDB ||
 window.mozIndexedDB ||
@@ -38,6 +40,25 @@ function checkDatabase() {
     const getAll = store.getAll();
 
     getAll.onsuccess = function () {
-        
-    }
+        if (getAll.result.length > 0) {
+            fetch("/api/transaction/bulk", {
+                method: "POST",
+                body: JSON.stringify(getAll.result),
+                header: {
+                    Accept: 'application/json, text/plain, */*',
+                    "Content-Type": "application/json"
+                }
+            })
+            .then(response => {
+                return response.json();
+            })
+            .then(() => {
+                const transaction = db.transaction(["pending"], "readwrite");
+                const store = transaction.objectStore("pending");
+                store.clear();
+            });
+        }
+    };
 }
+
+window.addEventListener('online', checkDatabase);
