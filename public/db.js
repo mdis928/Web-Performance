935e80accb837da4b598d80450e8f79498664336
Michael DiSanto <mdisanto928@gmail.com>
2021-07-20 20:05:31 -0400

    updated db.js. Offline should be working now

--- public/db.js
+++ public/db.js
@@ -1,15 +1,14 @@
-// const { response } = require("express");
-
 const indexedDB =
 window.indexedDB ||
 window.mozIndexedDB ||
 window.webkitIndexedDB ||
+window.msIndexedDB ||
 window.shimIndexedDB;
 
 let db;
-const request = indexedDB.open("budget, 1");
+const request = indexedDB.open("budget", 1);
 
-request.onupgradeneeded = ({ target}) => {
+request.onupgradeneeded = ({ target }) => {
     let db = target.result;
     db.createObjectStore("pending", { autoIncrement: true});
 };
@@ -32,33 +31,33 @@ function saveRecord(record) {
     const store = transaction.objectStore('pending');
 
     store.add(record);
-};
+}
 
 function checkDatabase() {
-    const transaction = db.transaction(["pending"], readwrite);
-    const store = transaction.objectStore('pending');
+    const transaction = db.transaction(["pending"], "readwrite");
+    const store = transaction.objectStore("pending");
     const getAll = store.getAll();
-
-    getAll.onsuccess = function () {
-        if (getAll.result.length > 0) {
-            fetch("/api/transaction/bulk", {
-                method: "POST",
-                body: JSON.stringify(getAll.result),
-                header: {
-                    Accept: 'application/json, text/plain, */*',
-                    "Content-Type": "application/json"
-                }
-            })
-            .then(response => {
-                return response.json();
-            })
-            .then(() => {
-                const transaction = db.transaction(["pending"], "readwrite");
-                const store = transaction.objectStore("pending");
-                store.clear();
-            });
-        }
+  
+    getAll.onsuccess = function() {
+      if (getAll.result.length > 0) {
+        fetch("/api/transaction/bulk", {
+          method: "POST",
+          body: JSON.stringify(getAll.result),
+          headers: {
+            Accept: "application/json, text/plain, */*",
+            "Content-Type": "application/json"
+          }
+        })
+        .then(response => {        
+          return response.json();
+        })
+        .then(() => {
+          const transaction = db.transaction(["pending"], "readwrite");
+          const store = transaction.objectStore("pending");
+          store.clear();
+        });
+      }
     };
-}
+  }
 
 window.addEventListener('online', checkDatabase);
--- public/service-worker.js
+++ public/service-worker.js
@@ -1,7 +1,7 @@
 const FILES_TO_CACHE = [
   '/',
-  '/index.html',
   '/index.js',
+  '/db.js',
   '/style.css',
   '/manifest.json',
   '/icons/icon-192x192.png',
